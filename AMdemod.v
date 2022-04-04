6db9f7e658f38d8de7bd7bbd000a06c767d86184
karlMarx1818 <91475814+karlMarx1818@users.noreply.github.com>
2022-04-04 19:31:17 +0800

    Add files via upload

--- AMdemod.v
+++ AMdemod.v
@@ -4,9 +4,9 @@ module AMdemod (
     input [15:0]carrier,      
     output [15:0]sig_out
 );
-    wire[31:0]sig_temp;wire[31:0]sig_temp2;
+    wire[31:0]sig_temp;wire[15:0]sig_temp2;
     wire[15:0]sig_in_s={!sig_in[15],sig_in[14:0]};
-    wire[15:0]carrier_s={!carrier[15],carrier[14:0]};、
+    wire[15:0]carrier_s={!carrier[15],carrier[14:0]};
 
     /*将以调制信号与载波信号相乘，积化和差可得：
       [(1+ma*sin(w_s*t))*sin(w_c*t)]*sin(w_c*t)=f[ sin(2w_c*t),sin((2w_c+w_s)*t),sin(w_s*t) ]
@@ -22,6 +22,6 @@ module AMdemod (
     .filter_out(sig_temp2)
     );
     //assign sig_out=sig_temp2[30:15]+32768;
-    assign sig_out=sig_temp2[15:0]+32768;
+    assign sig_out=sig_temp2+32768;
 
 endmodule
